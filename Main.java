a29281571b9dac3049ac324d5e2c824eb57f2868
JJMUNPER <jmunper007@g.educaando.es>
2023-01-30 19:49:58 +0100

    Ejercicio corregido

--- Main.java
+++ Main.java
@@ -6,15 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int [][] a = new int[10][3];
+        int [][][] a = new int [10][10][3];
         int num=2;
 
-        for (int i=0;i<a.length;i++){
+        for (int i=1;i<=10;i++){
 
             a [i-1] = metodoMultiplicacion(i);
         }
 
-        printTab(a);
+        imprimeTabla(a);
 
 
     }
@@ -32,15 +32,23 @@ public class Main {
         return multiplica;
     }
 
-    public static void printTab (int [][] tabla){
+    public static void crearTab (int [][] tabla){
 
         for (int i=0;i< tabla.length;i++){
-            System.out.print("Tabla del: "+ (i + 1));
             for (int j =0;j<tabla[i].length;j++){
-                System.out.print(Arrays.toString(tabla[i][j]));
+                System.out.print(tabla[i][j]);
             }
 
             System.out.println();
+        }
+    }
+
+    public static void imprimeTabla (int[][][] t){
+        for (int i=0;i<t.length;i++){
+            System.out.println("Tabla del " + (i+1));
+            for (int j=0;j<t[i].length;j++){
+                System.out.printf("%d x %d = %d %n", t[i][j][0], t[i][j][1], t[i][j][2]);
+            }
         }
     }
 
